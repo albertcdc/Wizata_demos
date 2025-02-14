34b8dad667ecb94ea1e983fe01c8397e7b83199a
Albert Comellas <aco@wizata.com>
2025-02-14 18:34:49 +0100

    Finished v1.1 with outlier detection and filtering Script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='wizata_demo_scripts',
     packages=find_packages(),
-    version='0.0.1',
+    version='0.0.2',
     description='Scripts to make Wizata demos.',
     author='aco',
     url='https://github.com/albertcdc/Wizata_demos'
